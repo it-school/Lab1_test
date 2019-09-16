d09885f161b537c9ba91611dcff79abef8cd8134
Viktor Speranskyy <it@osvita.od.ua>
2019-09-16 14:05:20 +0300

    v.2

--- Lab1_test/Program.cs
+++ Lab1_test/Program.cs
@@ -12,6 +12,9 @@ namespace Lab1_test
         static void Main(string[] args)
         {
             Console.WriteLine(kvadrat(20));
+            int k = 3;
+            for (int n = 1; n <= 5; n++)
+                Console.WriteLine((n * k) % 24 + 1);
         }
 
         static double kvadrat(double number)
