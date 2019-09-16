e0336bca7e439e8dc39c67366bed4d5b214d7ef0
Viktor Speranskyy <it@osvita.od.ua>
2019-09-16 14:00:19 +0300

    Add project files.

---
+++ Lab1_test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(kvadrat(20));
+        }
+
+        static double kvadrat(double number)
+        {
+            // return number * number;
+            return Math.Pow(number, 2);
+        }
+    }
+}
